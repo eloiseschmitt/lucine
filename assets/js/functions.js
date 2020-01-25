c02931c3aa8855c9495dd48c71a6a85678fd671b
Eloïse Schmitt <eloiseschmitt@iMac-de-Eloise.local>
2020-01-25 15:38:20 +0100

    Ajout au panier et modification de quantités

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,54 +3,35 @@ let nbItemInCart = 0;
 let $nbOfElements = $('.nbOfElements');
 
 const $cartResume = $('#cart-resume');
-let emptycart = 1; // 1 for yes, 0 for no
+let idsInCart = [];
+let idProductClicked = '';
 
-let $productImage = $('.product-image');
-let $productName = $('.product-name');
-let $productPrice = $('.product-price');
-
-let tableRow = `
-    <tr>
-        <td class="product-image"></td>
-        <td class="product-name"></td>
-        <td class="product-price"></td>
-        <td class="product-quantity">
-            <select>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-            </select>
-        </td>
-        <td class="product-total-price">8,00 €</td>
-        <td><i class="far fa-window-close"></i></td>
-    </tr>
-`;
-
+/* Add a product in cart or increment quantity */
 $('.btn-add-cart').on('click', function() {
-    // Increase number of items next icon in header
-    nbItemInCart++;
+    nbItemInCart++; // Increase number of items next icon in header
     $nbOfElements.text(nbItemInCart);
 
-    // if cart empty, create first row in modal
-    if(emptycart === 1) {
-        let productImage = $(this).parents('.product-wrapper').children('img').clone();
-        //$productImage.html(productImage);
+    idProductClicked = $(this).parents('.product-wrapper').children('.details-wrapper').children('h3').data('id'); // id product clicked
+    let productPrice = $(this).parents('.product-wrapper').children('.details-wrapper').children('p').data('price');
+    let optionSelected = ""; 
 
+    let idAndQuantity = {
+        id: idProductClicked,
+        quantity: 1,
+        price: +productPrice
+    }
+    let find = idsInCart.find(product => product.id == idAndQuantity.id); 
+    if(find == undefined) { // product id not already in cart, add it
+        idsInCart.push(idAndQuantity);
         let productName = $(this).parents('.product-wrapper').children('.details-wrapper').children('h3')[0].innerText;
-        //$productName.text(productName);
 
-        let productPrice = $(this).parents('.product-wrapper').children('.details-wrapper').children('p').data('price');
-        //$productPrice.text(productPrice+',00 €');
-
-        $cartResume.html(`
-            <tr>
-                <td class="product-image">${productImage}</td>
+        $cartResume.append(`
+            <tr data-id="${idProductClicked}">
+                <td class="product-image">${$(this).parents('.product-wrapper').children('img')[0].outerHTML}</td>
                 <td class="product-name">${productName}</td>
                 <td class="product-price">${productPrice},00 €</td>
                 <td class="product-quantity">
-                    <select>
+                    <select onchange="modifyQuantity(this, ${idProductClicked});">
                         <option selected>1</option>
                         <option>2</option>
                         <option>3</option>
@@ -62,12 +43,55 @@ $('.btn-add-cart').on('click', function() {
                 <td><i class="far fa-window-close"></i></td>
             </tr>
         `);
-        emptycart = 0;
     }
-    // verify if product reference already in cart
-    // if product not already in cart, create a row 
-    // else increment quantity
-
-    
-    
+    else {
+        find.quantity++; // else increment quantity
+        switch(find.quantity) {
+            case 1: 
+                optionSelected = `<option selected>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>`;
+            break;
+            case 2:
+                optionSelected = `<option>1</option>
+                <option selected>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>`;
+            break;
+            case 3:
+                optionSelected = `<option>1</option>
+                <option>2</option>
+                <option selected>3</option>
+                <option>4</option>
+                <option>5</option>`;
+            break;
+            case 4:
+                optionSelected = `<option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option selected>4</option>
+                <option>5</option>`;
+            break;
+            case 5:
+                optionSelected = `<option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option selected>5</option>`;
+        }
+        $('tbody').find(`tr[data-id=${idProductClicked}]`).children('td.product-quantity').children().html(optionSelected); // Modify option selected
+        $('tbody').find(`tr[data-id=${idProductClicked}]`).children('td.product-total-price').text(find.quantity*find.price+',00 €'); // Modify total price
+    }
+       
 });
+
+/* Modify quantity in cart by row */
+function modifyQuantity(val, idProductClicked) {
+    idProductClicked = idProductClicked;
+    let find = idsInCart.find(product => product.id == idProductClicked); 
+    find.quantity = parseInt(val.value);
+    $('tbody').find(`tr[data-id=${idProductClicked}]`).children('td.product-total-price').text(find.quantity*find.price+',00 €'); // Modify total price
+}
